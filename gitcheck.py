c23f871bd4817f6f89644a103e09e77acb69a2dd
ntat1903 <158596282+ntat1903@users.noreply.github.com>
2024-02-05 10:57:18 +0700

    Add files via upload

---
+++ gitcheck.py
@@ -0,0 +1,35 @@
+from math import sqrt
+
+print ("Giải phươnng trình bậc 2: ax^2 + bx + c = 0")
+
+a = float(input ("Nhập a: "))
+b = float(input ("Nhập b: "))
+c = float(input ("Nhập c: "))
+
+if a == 0:
+    if b == 0:
+        if c == 0:
+            print ("Phương trình có vô số nghiệm! ")
+        else:
+            print ("Phương trình vô nghiệm!")
+    
+    else:
+        if c == 0:
+            print ("Phương trình có 1 nghiệm x = 0!")
+        else: 
+            print ("Phương trình có 1 nghiệm x = ", -c/b)
+
+else:
+    delta = b**2 - 4*a*c
+    print ("delta = ", delta)
+    
+    if delta < 0:
+        print ("Phương trình vô nghiệm!")
+    
+    elif delta == 0:
+        print ("Phương trình có nghiệm kép x = y = ", -b/(2*a))
+    
+    else:
+        print ("Phương trình có 2 nghiệm phân biệt!")
+        print ("x = ", float((-b + sqrt(delta))/(2*a)))
+        print ("y = ", float((-b - sqrt(delta))/(2*a)))
